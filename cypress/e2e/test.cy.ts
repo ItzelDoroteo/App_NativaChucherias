9976509cb33ef705ccc807754f49c9cadc4c491b
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-24 11:53:40 -0600

    actualizacion de puerto en test e2e

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/');
+    // cy.intercept('GET', '/products').as('getProducts');
+    // cy.wait('@getProducts'); 
     // Espera a que el contenedor esté visible
     cy.get('#container', { timeout: 20000 }).should('be.visible');
     cy.get('#container').should('contain', 'Productos más vendidos');
