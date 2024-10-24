4c2762ec8bb279f2d8b0581abb6bd0d86c1e998a
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-24 10:36:07 -0600

    uodate cypress test

--- cypress/e2e/test.cy.ts
+++ cypress/e2e/test.cy.ts
@@ -1,6 +1,8 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
-    cy.visit('/')
+    cy.visit('/');
+    // Espera a que el contenedor esté visible
+    cy.get('#container', { timeout: 20000 }).should('be.visible');
     cy.get('#container').should('contain', 'Ready to create an app?');
-  })
-})
+  });
+});
