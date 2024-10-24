1ad1c5b46869c5651f6c6dbbd8fd4d2c81fa8412
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-24 10:30:47 -0600

    update test e2e - v2.0

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173",
+    baseUrl: "http://localhost:5173", // La URL base de tu aplicación
+    defaultCommandTimeout: 10000, // Aumenta el tiempo de espera por defecto a 10 segundos
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Puedes agregar eventos personalizados aquí, si es necesario
     },
+    viewportWidth: 1280,  // Ancho del viewport
+    viewportHeight: 720,  // Altura del viewport
+    video: false, // Deshabilita la grabación de video, si no es necesario
+    screenshotOnRunFailure: true // Habilita captura de pantalla en caso de fallo
   },
 });
