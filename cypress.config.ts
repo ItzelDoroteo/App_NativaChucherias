2067a2bbfddca62e69801f6780a1398279699e90
Jose Luis Hernandez De La Cruz <dareckluis@gmail.com>
2024-10-29 07:44:18 -0600

    update

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
-    baseUrl: "http://localhost:5173/", // La URL base de tu aplicación
+    baseUrl: "http://localhost:8100/", // La URL base de tu aplicación
     defaultCommandTimeout: 10000, // Aumenta el tiempo de espera por defecto a 10 segundos
     setupNodeEvents(on, config) {
       // Puedes agregar eventos personalizados aquí, si es necesario
